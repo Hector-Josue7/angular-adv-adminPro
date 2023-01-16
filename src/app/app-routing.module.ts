2f6f11ab2b49f72f8a4b4a47174024337e26edce
Hector <hectorardonm@gmail.com>
2023-01-15 19:11:42 -0600

    fin seccion 3 -diseño

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule, Component } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+
+// esta es la configuracion que angular haria si nosotros le indicamos que nos cree las rutas
+
+const routes: Routes = [
+{
+  path: '',
+  component: PagesComponent,
+  children:[
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'progress', component: ProgressComponent},
+    {path: 'grafica1', component: Grafica1Component},
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  ]
+},
+
+{path: 'register', component: RegisterComponent},
+{path: 'login', component: LoginComponent},
+
+{path: '**', component: NopagefoundComponent},  // cualquier ruta que no este definida arriba
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],   // forRoot es para rutas principales
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
