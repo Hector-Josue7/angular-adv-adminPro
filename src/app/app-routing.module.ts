ca9f78a4820f5485e942268939a94668d4bf290b
Hector <hectorardonm@gmail.com>
2023-03-12 21:33:44 -0600

    Fin sección 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,33 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
+//Modulos
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+
 
 // esta es la configuracion que angular haria si nosotros le indicamos que nos cree las rutas
 
 const routes: Routes = [
-{
-  path: '',
-  component: PagesComponent,
-  children:[
-    {path: 'dashboard', component: DashboardComponent},
-    {path: 'progress', component: ProgressComponent},
-    {path: 'grafica1', component: Grafica1Component},
-    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  ]
-},
 
-{path: 'register', component: RegisterComponent},
-{path: 'login', component: LoginComponent},
-
+  //path: '/dashboard' PagesRouting
+  //path: '/auth' AuthRouting
+  //path: '/medicos' MedicosRouting
+  //path: '/compras' ComprasRouting
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
 {path: '**', component: NopagefoundComponent},  // cualquier ruta que no este definida arriba
+
 ];
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],   // forRoot es para rutas principales
+  imports: [RouterModule.forRoot(routes),
+   PagesRoutingModule,
+   AuthRoutingModule
+  ],   // forRoot es para rutas principales
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
